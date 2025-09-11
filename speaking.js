6f480556b1011544fe2d7b687f0fcc2522d944ec
00001H <Programmer00001H@gmail.com>
2025-09-11 21:43:05 +0800

    speaking

---
+++ speaking.js
@@ -0,0 +1,87 @@
+"use strict";
+const nspeak = document.getElementById("nspeak");
+const nlisten = document.getElementById("nlisten");
+const proficiency = document.getElementById("proficiency");
+const situation = document.getElementById("situation");
+const description = document.getElementById("description");
+for (const e of document.getElementsByClassName("range-display")) {
+    const target = document.getElementById(e.getAttribute("for"));
+    const dw = e.getAttribute("data-width");
+    if (dw !== null) {
+        e.style.width = `${dw}ch`;
+        e.style.display = "inline-block";
+        e.style.textAlign = "right";
+    }
+    function update_field() {
+        if (target.value === target.max && e.getAttribute("data-over") === "over") {
+            e.textContent = `${target.valueAsNumber - 1}+`;
+        }
+        else {
+            e.textContent = target.value;
+        }
+    }
+    target.addEventListener("input", update_field);
+    update_field();
+}
+function update() {
+    const sc = nspeak.valueAsNumber;
+    const nc = nlisten.valueAsNumber;
+    let value;
+    if (sc) {
+        if (nc === 0) {
+            if (sc === 1) {
+                situation.textContent = "One-on-one discussion";
+                description.textContent = "I can speak pretty well. Sometimes I speak too much, though.";
+                value = 1.0;
+            }
+            else if (sc <= 8) {
+                situation.textContent = "Group discussion";
+                description.textContent = "Pretty active participation. No stress.";
+                value = 0.85;
+            }
+            else {
+                situation.textContent = "Classroom discussion";
+                description.textContent = "Okay participation.";
+                value = 0.85 - (sc - 8) / 110;
+            }
+        }
+        else {
+            if (sc <= 10) {
+                situation.textContent = "Group presentation";
+                description.textContent = "Pretty good performance. A bit nervous, though.";
+                value = 0.81;
+            }
+            else {
+                situation.textContent = "Chaotic group presentation";
+                description.textContent = "I've never experienced something like this before, and I'm not looking forward to it.";
+                value = 0.70;
+            }
+        }
+    }
+    else {
+        if (nc === 0) {
+            situation.textContent = "Monologue";
+            description.textContent = "Best performance, and feels great. I can go on non-stop for an hour.";
+            value = 1.3;
+        }
+        else if (nc === 1) {
+            situation.textContent = "Explaining to someone";
+            description.textContent = "Feels good, and I'm almost unstoppable.";
+            value = 1.1;
+        }
+        else if (nc <= 30) {
+            situation.textContent = "Classroom presentation";
+            description.textContent = "I can articulate myself clearly enough, though I sometimes pause and wonder if the audience understood what I said.";
+            value = 0.93;
+        }
+        else {
+            situation.textContent = "Public speech";
+            description.textContent = "I get way too excited and probably talk faster than I should.";
+            value = 1.28;
+        }
+    }
+    proficiency.textContent = `${(100 * value).toFixed(2)}%`;
+}
+nspeak.addEventListener("input", update);
+nlisten.addEventListener("input", update);
+update();
